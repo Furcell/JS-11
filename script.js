b6968df6007796aee0733bc6ce4c008e285a887e
Lashapurtseladze <149567152+Furcell@users.noreply.github.com>
2024-04-18 12:16:57 +0400

    add slider

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 "use strict";
+
+// HomeWork-11
+
+const mySlider = new Splide("#mySlider",{
+    perPage: 2,
+    gap: "30px",
+    pagination: false,
+    breakpoints: {
+        1024: {
+            perPage: 1
+        },
+        768: {
+            arrows: false,
+            pagination: true
+        }
+    }
+})
+
+mySlider.mount()
