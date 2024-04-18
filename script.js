2c220413f504866aba36122c33e8e653cc37eb44
Lashapurtseladze <149567152+Furcell@users.noreply.github.com>
2024-04-18 12:30:04 +0400

    finish JS10

--- script.js
+++ script.js
@@ -3,16 +3,14 @@
 // HomeWork-11
 
 const mySlider = new Splide("#mySlider",{
-    perPage: 2,
+    perPage: 3,
     gap: "30px",
-    pagination: false,
     breakpoints: {
         1024: {
             perPage: 1
         },
         768: {
             arrows: false,
-            pagination: true
         }
     }
 })
